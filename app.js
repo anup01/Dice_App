7534ff169c3f91e8a29f6bb0bee3538afc3aeec1
anup <sableanup999@gmail.com>
2019-12-05 13:34:16 +0530

    set the new game functionality

--- app.js
+++ app.js
@@ -10,17 +10,9 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer;
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
 
+init();
 
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     // 1. Random number
@@ -79,8 +71,28 @@ function nextPlayer() {
         document.querySelector('.dice').style.display = 'none';
 }
 
-
-
+// for new game
+document.querySelector('.btn-new').addEventListener('click', init);
+ 
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    // Hide dice initially
+    document.querySelector('.dice').style.display = 'none';
+    // set score and global score 0 initially
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add ('active');
+}
 
 
 
